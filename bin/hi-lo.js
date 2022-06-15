895b7cb64265897859efc9279ed53f4e841aa817
Brandon <3292898+brangor@users.noreply.github.com>
2022-06-15 18:21:34 +1200

    Mostly validating

--- bin/hi-lo.js
+++ bin/hi-lo.js
@@ -1,36 +1,38 @@
 #!/usr/bin/env node
 
-let prompt = require('prompt-sync')();
+let prompt = require('prompt-sync')({
+  sigint: true,
+});
 
 const RANDOM_VAL_MIN = 1;
 const RANDOM_VAL_MAX = 100;
 const MAX_GUESSES = 6;
 
 let intro_text = [
-  '~~~~~~~~~~~HI~~~LO~~~~~~~~~~~',
+  '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~HI~~~LO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
   '',
   ' Aim of the game:',
-  `  • I'll pick a number between ${RANDOM_VAL_MIN} and ${RANDOM_VAL_MAX}`,
-  `  • You'll be able to guess the mystery number ${MAX_GUESSES} times`,
-  `  • I'll let you know whether you're HI/LO compared to the actual number`
+  `  ~ I'll pick a number between ${RANDOM_VAL_MIN} and ${RANDOM_VAL_MAX}`,
+  `  ~ You'll be able to guess the mystery number ${MAX_GUESSES} times`,
+  `  ~ I'll let you know whether you're HI/LO compared to the actual number`
 ];
 
-const END_TEXT = [
+let END_TEXT = [
   '',
   ' Thanks for Playing!     ',
   '',
-  '~~~~~~~~~~~HI~~~LO~~~~~~~~~~~'
+  '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~HI~~~LO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 ]
 
-const NEXT_GUESS_PROMPT_TEXT = " GUESS: ";
-const GUESS_IS_LOW_TEXT =   '  • LO';
-const GUESS_IS_HIGH_TEXT =  '  • HI';
+const NEXT_GUESS_PROMPT_TEXT = " GUESS ≈ ";
+const GUESS_IS_LOW_TEXT =   '  ~ LO';
+const GUESS_IS_HIGH_TEXT =  '  ~ HI';
 
 let mystery_number = Math.floor(
     Math.random() * RANDOM_VAL_MAX
   ) + RANDOM_VAL_MIN;
 
-var game_won = false;
+let game_won = false;
 
 let guess_history = [];
 
@@ -38,13 +40,34 @@ for (var line of intro_text) { console.log(line) }
 
 while (!game_won && guess_history.length < MAX_GUESSES) {
   console.log('');
-  // validation start
-  //let valid = false
-  //while (!valid) {
-  //  valid = true;
-  //}
+
+  let guess = parseInt(prompt(` GUESS #${guess_history.length + 1} ≈ `, -1));
+
+  let valid = false
+  while (!valid) {
+    let issues = [];
+
+    if (!Number.isInteger(guess)) {
+      issues.push("Guess needs to be an integer.");
+    }
+
+    if (guess < 1 || guess > 100) {
+      issues.push(`Guess outside range ${RANDOM_VAL_MIN}-${RANDOM_VAL_MAX}`);
+    }
+
+    if (guess_history.includes(guess)) {
+      issues.push("Guess already done been guessed.");
+    }
+
+    if (issues.length == 0) {
+      valid = true;
+    } else {
+      for(var issue of issues) { console.log('  ≈ERROR≈ ' + issue) }
+      console.log('');
+      guess = parseInt(prompt(`  GUESS #${guess_history.length + 1} ≈ `, -1));
+    }
+  }
   // validation end
-  let guess = prompt(NEXT_GUESS_PROMPT_TEXT);
 
   if (guess > mystery_number) {
     console.log(GUESS_IS_HIGH_TEXT);
